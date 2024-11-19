ec43a6c07ede986a6f0e5d8d593fa700e777a660
Junya Michanan <michanaj@gmail.com>
2024-11-19 08:32:30 +0700

    Added data files

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,16 @@ import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat App powered by GPT-4o mini and LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Chat App powered by GPT-4o mini and LlamaIndex 💬🦙")
+#st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
+            "content": "Ask me a question about Thailand Succession Law!",
         }
     ]
 
@@ -21,14 +21,13 @@ def load_data():
     reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
     docs = reader.load_data()
     Settings.llm = OpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0.2,
-        system_prompt="""You are an expert on 
-        the Streamlit Python library and your 
-        job is to answer technical questions. 
+        model="gpt-4o-mini", #"gpt-4-turbo",
+        temperature=0.1,
+        system_prompt="""You are an expert on Thailand Succession Law and your 
+        job is to answer any detailed question about the Succession Law of Thailand. 
         Assume that all questions are related 
-        to the Streamlit Python library. Keep 
-        your answers technical and based on 
+        to the law. Keep 
+        your answers clear, precise and based on 
         facts – do not hallucinate features.""",
     )
     index = VectorStoreIndex.from_documents(docs)
